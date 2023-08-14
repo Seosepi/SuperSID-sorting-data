b362d8cb004039e1fbf3d90bb1bc010541de9de1
Seosamh McAteer <138021660+Seosepi@users.noreply.github.com>
2023-08-14 10:55:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
-csv_location = '/Users/Administrator.ADMINTR-8ULO5FO/OneDrive/VLF_folder/vlf_transmitters.csv'
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 13:21:53 2023
+
+@author: dunsink
+"""
+
+#Insert used station names and associated frequency in the respective lists below.
+#Ensure every station and associated frequency hold the same position in their respective lists
+stations = ["GBZ","DHO38","NAA","FTA","NDT"]
+frequencies = ["19580","23400","24000","20900","22200"]
+
+#rawfolder is the folder from which the raw data is being read and processed
+#sortedfolder is the folder for where the now processed data will be now stored and sorted
+rawfolder = '/home/dunsink/supersid/Data/'
+sortedfolder = "/home/dunsink/supersid/Data/vlf"
